2f07e08bfa834137cc429bc8fe25987927990fd1
mlanies <91416159+mlanies@users.noreply.github.com>
2025-02-09 15:38:20 +0300

    update

--- win/app.py
+++ win/app.py
@@ -1,5 +1,6 @@
+import os
 import sys
-
+import subprocess
 import psutil
 from PyQt5.QtWidgets import QApplication
 
@@ -8,11 +9,21 @@ from views import MainView, Connector
 from db.db_connector import SqliteDBConnect
 
 
-class App(QApplication):
-    """
-        Класс для запуска приложения.
-    """
+def get_cloudflared_path():
+    """Определяет путь к cloudflared.exe в зависимости от режима работы."""
+    if getattr(sys, 'frozen', False):
+        # PyInstaller упаковывает файлы в _MEIPASS
+        base_path = sys._MEIPASS
+    else:
+        # В режиме разработки используется обычный путь
+        base_path = os.path.dirname(__file__)
 
+    return os.path.join(base_path, "cloudflared.exe")
+
+
+class App(QApplication):
+    """Класс для запуска приложения."""
+    
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
         version = "2.0.0"
@@ -23,28 +34,30 @@ class App(QApplication):
         self.connect_view = Connector(version, url_views, site_url)
         self.controller = Controller(self.model, self.main_view, self.connect_view)
 
+        # Запуск cloudflared
+        cloudflared_path = get_cloudflared_path()
+        if os.path.exists(cloudflared_path):
+            subprocess.Popen([cloudflared_path, "tunnel", "--url", "http://localhost:5000"])
+        else:
+            print(f"Ошибка: cloudflared.exe не найден по пути {cloudflared_path}")
+
         self.main_view.show()
         self.main_view.setWindowTitle("2GC Free")
 
 
 def pre_init():
+    """Проверяет, не запущено ли уже приложение."""
     try:
-        # Получаем список всех запущенных процессов
         processes = psutil.process_iter(['pid', 'name'])
-
-        # Подсчитываем количество запущенных процессов с именем "2gc.exe"
         count = sum(1 for proc in processes if proc.info['name'] == "2gc.exe")
-        if count > 2:
-            return False
-        return True
-
+        return count <= 2
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Ошибка: {e}")
+        return False
 
 
 if __name__ == '__main__':
-    pi = pre_init()
-    if pi:
+    if pre_init():
         app = App(sys.argv)
         app.setQuitOnLastWindowClosed(False)
         sys.exit(app.exec_())
